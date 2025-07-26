ca886ee4e6d1c80425bdad260d3a163e7802df26
Kenichi Kamiya <kachick1@gmail.com>
2025-07-26 21:02:43 +0900

    `cargo fmt`

--- src/package_checker.rs
+++ src/package_checker.rs
@@ -36,10 +36,7 @@ impl std::fmt::Display for PackageCheckResult {
                 write!(f, "\x1b[31m[FAILURE]\x1b[0m: log={log_url}")
             }
             PackageCheckResult::LogNotFound { log_list_url } => {
-                write!(
-                    f,
-                    "\x1b[33m[WARN]\x1b[0m: No logs found at {log_list_url}"
-                )
+                write!(f, "\x1b[33m[WARN]\x1b[0m: No logs found at {log_list_url}")
             }
             PackageCheckResult::Skip { log_url } => {
                 write!(f, "\x1b[33m[WARN]\x1b[0m: Skipped log={log_url}")
